ae7e53f3410dd7186233591318c9b9d53943e30f
Katlin Anderson and Kaya Jepson <katlin@>
2019-04-15 14:24:40 -0700

    add player avatars to img folder

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,16 @@
 $(function(){
 
-  var newGame = new Game();
+  // var newGame = new Game();
   $('form').submit(function(e) {
    e.preventDefault();
    for (let i = 1; i <= 4; i++) {
      var name = $('input#name' + i).val();
-     var player = new Player(name);
-     //assign role?
+     // var player = new Player(name);
+     // player.assignRole();
    }
-
-   var size = $('input:radio[name=size]:checked').val();
-   var sauce = $('input:radio[name=sauce]:checked').val();
-   var cheese = $('input:radio[name=cheese]:checked').val();
-   var toppings = []; $('input:checkbox[name=topping]:checked').each(function(){
-     toppings.push($(this).val());
+   // window.location.href = "../werewolf-clone/night.html"
   });
+
+  // $('.img').html('<img src="img/player' + i + '.png" alt="an avatar for player '+ i + '">')
+
 });
