82af5712090f87f11e871c7e23dc18c444fb583b
Colton Lacey, Justin Kerntz, Katlin Anderson and Kaya Jepson <colton@>
2019-04-15 16:36:57 -0700

    add timer function

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,8 @@ function Game() {
 
 function Player(name,roleId) {
   this.name = name,
-  this.roleId = roleId
+  this.roleId = roleId,
+  this.playerStatus = true
 }
 
 Game.prototype.addPlayer = function(player){
@@ -45,3 +46,30 @@ Game.prototype.randomRoles = function(playerNumber) {
 
 newGame.randomRoles(4);
 //Above starts game and assigns initial properties to players
+
+
+//Countdown functions
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+            alert("Count Down Done!!");
+            votePhase();
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var twoMinute = 100000 * 2,
+        display = document.querySelector('#time');
+    startTimer(twoMinute, display);
+};
