f3eaa196dc0438f99e8463c2d3aabc2c6fb7d666
jk <kerntzj@gmail.com>
2019-04-16 09:02:20 -0700

    add player prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,46 @@
-var newGame = new Game();
+var player1 = new Player("qw")
+var player2 = new Player("as")
+var player3 = new Player("zx")
+var player4 = new Player("nm")
+var newGame = new StartGame(4);
 
-var player1 = new Player("one");
-var player2 = new Player("two");
-var player3 = new Player("three");
-var player4 = new Player("four");
-
-function Game() {
-  this.players = [],
-  this.roleIds = [],
-  this.currentId = -1
+StartGame.prototype.randomRoles = function(numberOfPlayers) {
+  for (var i = 1; i <= numberOfPlayers; i++) {
+    var randomRole = Math.floor(Math.random()* Math.floor(numberOfPlayers));
+    if (this.roleIds.includes(randomRole)) {
+      i --
+    }else{this.roleIds.push(randomRole)
+    }
+  }
 }
 
-function Player(name,roleId) {
+function StartGame(numberOfPlayers) {
+  this.players = [],
+  this.roleIds = randomRoles(numberOfPlayers),
+  this.numberOfPlayers = numberOfPlayers
+}
+
+function Player(name) {
   this.name = name,
-  this.roleId = roleId,
+  this.playerId = 0,
+  this.roleId = "",
+  this.voteCount = 0,
   this.playerStatus = true
 }
 
-Game.prototype.addPlayer = function(player){
-    player.id = this.assignId();
-    this.players.push(player);
+Player.prototype.addPlayer = function () {
+  this.playerId += newGame.players.length;
+  newGame.players.push(this);
+  this.assignRole();
 }
 
-Game.prototype.assignId = function() {
-  this.currentId += 1;
-  return this.currentId;
-}
-
-Game.prototype.assignRole = function() {
-  for (var i = 0; i <= newGame.currentId; i++) {
-    if (newGame.players[i].id === newGame.roleIds[0]) {
-      newGame.players[i].roleId = "Bug";
-    }else {newGame.players[i].roleId = "Dev"};
+Player.prototype.assignRole = function() {
+  for (var i = 0; i <= this.playerId; i++) {
+    if (this.playerId === newGame.roleIds[0]) {
+      this.roleId = "Bug";
+    }else {this.roleId = "Dev"};
   }
 }
-
-Game.prototype.randomRoles = function(playerNumber) {
-  for (var i = 1; i <= playerNumber; i++) {
-    var randomRole = Math.floor(Math.random()* Math.floor(playerNumber));
-    if (newGame.roleIds.includes(randomRole)) {
-      i --
-    }else{this.roleIds.push(randomRole)}
-  }
-}
-
-newGame.randomRoles(4);
 //Above starts game and assigns initial properties to players
 
 
@@ -68,8 +64,16 @@ function startTimer(duration, display) {
     }, 1000);
 }
 
-window.onload = function () {
-    var twoMinute = 100000 * 2,
+$(document).ready(function() {
+  $("#discussion").on("click", function() {
+
+    var twoMinute = 60* 2,
         display = document.querySelector('#time');
     startTimer(twoMinute, display);
-};
+  })
+})
+// window.onload = function () {
+//     var twoMinute = 60* 2,
+//         display = document.querySelector('#time');
+//     startTimer(twoMinute, display);
+// };
