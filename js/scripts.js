da18428ee23d3060c299d97db55a811c1d669b03
Katlin Anderson and Kaya Jepson <kaya@>
2019-04-16 10:43:31 -0700

    merge and clean up ui and business logics

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
+var newGame = new StartGame(4);
 
 function StartGame(numberOfPlayers) {
   this.players = [],
-  this.roleIds = randomRoles(numberOfPlayers),
+  this.roleIds = [],
   this.numberOfPlayers = numberOfPlayers
 }
 
@@ -10,7 +11,7 @@ StartGame.prototype.randomRoles = function(numberOfPlayers) {
     var randomRole = Math.floor(Math.random()* Math.floor(numberOfPlayers));
     if (this.roleIds.includes(randomRole)) {
       i --
-    }else{this.roleIds.push(randomRole)
+    } else{this.roleIds.push(randomRole)
     }
   }
 }
@@ -60,28 +61,30 @@ function startTimer(duration, display) {
 }
 
 $(function(){
-var location = 0
-  // var newGame = new StartGame(#ofplayers);
+  var location = 0
+  var retrievedNewGame = localStorage.getItem('newGame');
   $('form').submit(function(e) {
    e.preventDefault();
+   newGame.randomRoles(4);
    for (let i = 1; i <= 4; i++) {
      var name = $('input#name' + i).val();
-     // var player = new Player(name);
-     // newGame.addPlayer();
+     var player = new Player(name);
+     player.addPlayer();
    }
-   // newGame.assignRole(4);
-
+   localStorage.setItem('newGame', JSON.stringify(newGame));
    window.location.href = "../werewolf-clone/night.html"
   });
+
 
   // $('.img').html('<img src="img/player' + i + '.png" alt="an avatar for player '+ i + '">')
 
   $('button.next-role').on('click',function(){
+    console.log(JSON.parse(retrievedNewGame));
     console.log('you got here');
     $('#night-intro').hide();
     $('#night-begin-roles #night-player-intros').show();
     //This playerTurn array should actually be newGame.roleIds
-    $('span.img').html('<img src="img/player' + playerTurn[location] + '.png" alt="an avatar for player" id="player-' + playerTurn[location] + '-img">');
+    // $('span.img').html('<img src="img/player' + playerTurn[location] + '.png" alt="an avatar for player" id="player-' + playerTurn[location] + '-img">');
 
   });
 
@@ -89,27 +92,27 @@ var location = 0
     $('button#continue-night').show();
   });
 
-  $('button.continue-role').on('click', function() {
-    //in real life playerTurn here should be newGame.player v
-    if (playerTurn[location].role === 'Dev') {
-      $('#night-begin-roles').hide();
-      $('#developer').show();
-      location++;
-    } else if (playerTurn[location].role === 'Bug') {
-      $('#night-begin-roles').hide();
-      $('#bug').show();
-      location++;
-    } else if (!playerTurn[location].role) {
-      location = 0;
-      $('#night-player-intros').hide();
-      $('#night-end-roles').show();
-    }
-  })
+  // $('button.continue-role').on('click', function() {
+  //   //in real life playerTurn here should be newGame.player v
+  //   if (playerTurn[location].role === 'Dev') {
+  //     $('#night-begin-roles').hide();
+  //     $('#developer').show();
+  //     location++;
+  //   } else if (playerTurn[location].role === 'Bug') {
+  //     $('#night-begin-roles').hide();
+  //     $('#bug').show();
+  //     location++;
+  //   } else if (!playerTurn[location].role) {
+  //     location = 0;
+  //     $('#night-player-intros').hide();
+  //     $('#night-end-roles').show();
+  //   }
+  // })
 
   $('button#begin-day').on('click', function() {
     window.location.href = "../werewolf-clone/day.html"
   })
-  
+
   $("#discussion").on("click", function() {
 
     var twoMinute = 60* 2,
